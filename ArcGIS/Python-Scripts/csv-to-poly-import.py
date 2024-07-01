070a94668387f994b11cca3498ff704b10633b19
Michael McCain <michaelmc1989@gmail.com>
2024-07-01 17:37:05 -0400

    Added project file, new images, and comments to python script.

--- ArcGIS/Python-Scripts/csv-to-poly-import.py
+++ ArcGIS/Python-Scripts/csv-to-poly-import.py
@@ -1,3 +1,5 @@
+# Code from https://community.esri.com/t5/arcgis-pro-questions/how-to-convert-wkt-polygon-data-in-csv-file-into/td-p/1304292
+
 import arcpy
 import csv
 import os
@@ -9,6 +11,7 @@ out_gdb = r"output-.gdb-project"
 out_name = "output-name"
 out_fc_path = os.path.join(out_gdb, out_name)
 
+# Change spatial source as needed
 source_sr = arcpy.SpatialReference(4269)  # Source spatial reference NAD83
 target_sr = arcpy.SpatialReference(4326)  # ArcGIS Topography Map WGS1984
 
@@ -37,4 +40,3 @@ with open(input_csv, "r", newline="") as csvfile:
             except Exception as e:
                 print(f"Failed to convert or project geometry: {wkt_geom}; Error: {str(e)}")
 
-# Code from https://community.esri.com/t5/arcgis-pro-questions/how-to-convert-wkt-polygon-data-in-csv-file-into/td-p/1304292
